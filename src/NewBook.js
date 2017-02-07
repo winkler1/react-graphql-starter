88322bdce29fd84356ca3f0084549684c54eb241
Jeff Winkler <winkler1@gmail.com>
2017-02-07 11:51:39 -0500

    First pass
    Why:
    
    * Show react-router, GraphQL queries with params

---
+++ src/NewBook.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewBook extends Component {
+  render() {
+    return (
+      <div>
+        New goes here
+      </div>
+    );
+  }
+}
+
+export default NewBook;
